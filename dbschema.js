317f762366d9cb51eed2115b5ae9bd685db05b36
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-06-19 16:05:44 +0200

    Initial commit & app already started

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  posts: [
+    {
+      userHandle: 'user',
+      body: 'this is the post body',
+      createdAt: '2020-06-18T13:36:27.696Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
